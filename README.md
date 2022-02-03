fe7efb8b1af6c08a444661527f393c781e0b50cb
Arnav Daultani <arnavdaultani@gmail.com>
2022-02-03 14:16:18 +0530

    Done

---
+++ README.md
@@ -0,0 +1,119 @@
+# B-baka
+
+A simple and lightweight web framework for begginers.
+
+## Config
+
+There are 4 config options.
+
+- `hidebymessage`
+- `errorlogging`
+- `hostname`
+- `port`
+
+The first two are `bool`'s, the third one is a string and the fourth an integer.
+
+They can be change by doing the following
+
+```py
+from baka import Config
+Config.hidebymessage = False
+Config.errorlogging = False
+Config.hostname =  "hostnamehere"
+Config.port = "4000"
+```
+
+# Baka
+
+There are only 5 simple functions.
+
+## - `add_path`
+
+### Adds a certain endpoint to the path list
+
+```py
+add_path("/")
+add_path("/api")
+add_path("/home")
+add_path("/login")
+```
+ 
+## - `add_path_type`
+
+### Add the type of the path
+
+```py
+add_path_type("/" , "html")
+add_path_type("/api" , "json")
+add_path_type("/login" , "html")
+```
+
+## - `add_render`
+
+### Used to render things
+
+```py
+add_render("/" , render_template("index.html"))
+```
+
+```py
+add_render("/" , "<h1>Hello!</h1>")
+```
+
+```py
+a = {
+    "hello" : "b",
+    "wow" : "dang lol"
+    }
+add_render("/test" , a)
+```
+
+## - `render_template`
+
+### Used to convert html to bytes
+
+```py
+add_render("/" , render_template("index.html"))
+```
+
+## - `run`
+
+### Run the APP
+
+#### Just call it :p
+
+```py
+run()
+```
+
+# Types
+
+- `html`
+- `json`
+- `special`
+
+# Example
+
+```py
+from baka import run , add_path , add_path_type , add_render , render_template
+from baka import Config
+
+Config.hibyemessage = False
+
+add_path("/")
+add_path_type("/" , "html")
+add_render("/" , render_template("index.html"))
+
+add_path("/favicon.ico")
+add_path_type("/favicon.ico", "special")
+add_render("/favicon.ico" , render_template("fav.html"))
+
+add_path("/test")
+add_path_type("/test" , "json")
+a = {
+    "hello" : "b",
+    "wow" : "dang lol"
+    }
+add_render("/test" , a)
+run()
+```
---
+++ baka.py
@@ -0,0 +1,113 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import random
+import json
+
+class StartsWithError(Exception):
+    pass
+
+class Config:
+    hibyemessage = True
+    errorlogging = True
+    hostname = "localhost"
+    port = 8080
+
+class Baka(BaseHTTPRequestHandler):
+    global pathlist
+    global patht
+    global torender
+    global himessage
+    global byemessage
+
+    byemessage = ["why awe you weaving *looks at you* me :( bye bye" , "pwease dont go" , "bye f-f-fwen" , "dont go wuv"]
+    himessage = ["hi sewvews wunnying nyow *whispers to self* :p *runs away*" , "omg hi how awe y-you!!11" , "whats up f-f-fwen"]
+
+    pathlist = []
+    patht = []
+    foo = []
+    torender = []
+
+    def do_GET(self):
+        pathtype = None
+        if self.path in pathlist:
+
+            for item in patht:
+                try:
+                    pathtype = item[self.path]
+                    break;
+                except:
+                    pass
+            for i in torender:
+                try:
+                    value = i[self.path]
+                    break;
+                except:
+                    pass
+
+            if pathtype == "html":
+                self.send_response(200)
+                self.send_header("Content-type" , "text/html")
+                self.end_headers()
+                try:
+                    self.wfile.write(bytes(value , "utf-8"))
+                except:
+                    pass
+            elif pathtype == "special":
+                self.send_response(200)
+                self.send_header("Content-type" , "text/html")
+                self.end_headers()
+                try:
+                    self.wfile.write(bytes(value , "utf-8"))
+                except:
+                    pass
+            elif pathtype == "json":
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+                self.wfile.write(str(value).encode(encoding='utf_8'))
+            else:
+                if Config.errorlogging:
+                    logging.log(40 , f" Error '{self.path}', can't find path type '{pathtype}'")
+                self.send_response(400)
+                self.send_header("Content-type" , "text/html")
+                self.end_headers()
+                self.wfile.write(bytes("<h1>Error!</h1>" , "utf-8"))
+        else:
+            self.send_response(404)
+            self.send_header("Content-type" , "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<h1>404 Not found</h1>" , "utf-8"))
+            if Config.errorlogging:
+                logging.log(40 , f"Error path {self.path} not found")
+
+def add_path(pathy : str):
+    if pathy.startswith("/"):
+        pathlist.append(pathy)
+    else:
+        raise StartsWithError("Path must start with '/' ")
+
+def add_path_type(pathy : str , typey : str):
+    patht.append({pathy : typey})
+
+def add_render(pathy : str , daty : str):
+    torender.append({pathy : daty})
+
+def render_template(filename : str):
+    with open(filename , "r") as file:
+        return file.read()
+
+def run():
+        hostName = Config.hostname
+        serverPort = Config.port
+        webServer = HTTPServer((hostName, serverPort), Baka)
+        if Config.hibyemessage: print(random.choice(himessage))
+        print("Server started at http://%s:%s" % (hostName, serverPort))
+
+        try:
+            webServer.serve_forever()
+        except KeyboardInterrupt:
+            pass
+
+        webServer.server_close()
+        if Config.hibyemessage: print(random.choice(byemessage))
+        print("Server stopped.")
